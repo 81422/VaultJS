ee86c9c1376fb8eff0d83d29f3557c6e66b5f9b4
81422 <43666964+81422@users.noreply.github.com>
2019-01-16 09:21:55 +0100

    Add files via upload
    Update 16-1-19

--- vault/js/main.js
+++ vault/js/main.js
@@ -3,6 +3,10 @@ var clickCounter = 0;
 var numberOne;
 var numberTwo;
 var numberThree;
+var correctCodeBlock = document.getElementById("correctcodelight");
+var wrongCodeBlock = document.getElementById("wrongcodelight");
+var intervalTimer = 0;
+var codeButtons = document.getElementsByClassName("codebuttons");
 
 function getNumber(clickedButton) 
 {
@@ -20,6 +24,81 @@ function getNumber(clickedButton)
     else
     {
         numberThree = clickedButton.value;
+    }
+
+    if(clickCounter == 3)
+    {
+        for(i=0; i < codeButtons.length; i++) 
+        {
+            codeButtons[i].setAttribute('disabled', 'disabled');
+        }
+
+        if(numberOne == 3 && numberTwo == 2 && numberThree == 2)
+        {
+            outputcode.innerHTML = "Correct code";
+            var correctBlink = setInterval(function() 
+            {
+                intervalTimer++;
+
+                if (correctCodeBlock.style.visibility == 'hidden')
+                {
+                    correctCodeBlock.style.visibility = 'visible';
+                }
+                else
+                {
+                    correctCodeBlock.style.visibility = 'hidden';
+                }
+
+                if(intervalTimer == 10)
+                {
+                    clearInterval(correctBlink);
+                    for(i=0; i < codeButtons.length; i++) 
+                    {
+                        codeButtons[i].removeAttribute('disabled');
+                    }
+                    outputcode.innerHTML = "";
+                    codeContainer.innerHTML = "";
+                    clickCounter = "";
+                    numberOne = "";
+                    numberTwo = "";
+                    numberThree = "";  
+                    intervalTimer = 0;    
+                }
+            }, 500);
+        }
+        else
+        {
+            outputcode.innerHTML = "Wrong code";
+            var wrongBlink = setInterval(function() 
+            {
+                intervalTimer++;
+
+                if (wrongCodeBlock.style.visibility == 'hidden')
+                {
+                    wrongCodeBlock.style.visibility = 'visible';
+                }
+                else
+                {
+                    wrongCodeBlock.style.visibility = 'hidden';
+                }
+
+                if(intervalTimer == 10)
+                {
+                    clearInterval(wrongBlink);
+                    for(i=0; i < codeButtons.length; i++) 
+                    {
+                        codeButtons[i].removeAttribute('disabled');
+                    }
+                    outputcode.innerHTML = "";
+                    codeContainer.innerHTML = "";
+                    clickCounter = "";
+                    numberOne = "";
+                    numberTwo = "";
+                    numberThree = ""; 
+                    intervalTimer = 0;    
+                }
+            }, 500);
+        }
     }
 
     console.log(numberOne);
